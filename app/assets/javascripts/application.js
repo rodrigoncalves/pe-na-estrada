5ece1c435ed93b40a6bc66dc8d653b6e29812489
Leonardo Cambraia <leoccambraia@yahoo.com.br>
2014-09-26 19:57:49 -0300

    Stable map without form (dojo) Signed-off-by: Italo <italosensei@hotmail.com> Signed-off-by: Emilie <emilie.mo@hotmail.com> Signed-off-by: Hugo <hugomartins_15@hotmail.com> Signed-off-by: Tiago <thiagoribeironiquel@hotmail.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require underscore
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,6 +1,46 @@
+#-*- encoding : utf-8 -*-
+
 class RoutesController < ApplicationController
 
-	def index
+
+	def new
+  		@route = Route.new
 	end
 
+	def create
+		@route = Route.new(origin_params)
+
+		@my_test = origin_params
+
+		puts ">>>>>>>>>>>>>>>>>>>>>>>"
+		puts @my_test
+		puts ">>>>>>>>>>>>>>>>>>>>>>>"
+
+		render :new
+	
+  	end
+
+  	def show
+  		@route = Route.find_by_id(params[:id].to_i)
+  	end
+
+  	def index
+
+  		puts ">>>>>>>>>>>>>>>>"
+  		
+  		puts = " >>>> AQUI #{@test_origem}"
+
+  		puts ">>>>>>>>>>>>"
+  		
+		@routes = Route.all
+	end
+  	
+	def origin_params
+  		params.require(:route).permit(:origin, :destination)
+  	end
+ 	
+	
+
+
 end
+
