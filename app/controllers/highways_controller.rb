dfe65641080cdf56912deed92960bef31b8e5beb
italopaiva <italosensei@hotmail.com>
2014-09-28 20:49:46 -0300

    Search for a highway implemented

--- app/controllers/highways_controller.rb
+++ app/controllers/highways_controller.rb
@@ -1,7 +1,10 @@
 class HighwaysController < ApplicationController
 
 	def index
+
 		@highways = Highway.all
+		@highways2 = Highway.new
+		
 	end
 
 	def show
@@ -10,9 +13,26 @@ class HighwaysController < ApplicationController
 	def new
 	end
 
+	def create
+
+		@highways = Highway.all
+
+		@highways2 = Highway.new(origin_params)
+
+		@highwayInformedByUser = @highways2.idBr
+
+		render :index
+
+  	end
+
 	def import
 		Highway.import(params[:file])
 		redirect_to highways_path, notice: "Dados das rodovias importados com sucesso!"
 	end
 
+	def origin_params
+  		
+  		params.require(:highway).permit(:idBr)
+  		
+  	end
 end
