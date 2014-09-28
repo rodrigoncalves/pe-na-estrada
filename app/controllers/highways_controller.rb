8b0ea6c5f9f4722072c8e6515ec277d6aa3cf60f
thyago241 <thiagoribeironiquel@hotmail.com>
2014-09-28 17:02:44 -0300

    "Highway form for searchBR" signed-off-by: Tiago <thiagoribeironiquel@hotmail.com> signed-off-by: Lucas <hestorn.magno@hotmail.com>

--- app/controllers/highways_controller.rb
+++ app/controllers/highways_controller.rb
@@ -1,18 +1,33 @@
 class HighwaysController < ApplicationController
 
-	def index
-		@highways = Highway.all
-	end
-
-	def show
-	end
+	
 
 	def new
+		@highway = Highway.new
+	end
+
+	def create
+		
+		@highway = Highway.new(origin_params)
+
+		#@test_pesquisa = @highway.search
+
+		@highwayInformedByUser = origin_params
+
+		render :new
+	
+  	end
+
+	def index
+		@highways = Highway.all
 	end
 
 	def import
 		Highway.import(params[:file])
 		redirect_to highways_path, notice: "Dados das rodovias importados com sucesso!"
 	end
+	def origin_params
+  		params.require(:highway).permit(:idBr)
+  	end
 
 end
