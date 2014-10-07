102fea743105174cacf5f8858b25ebd6cfc0e1d5
Emilie Morais <emilie.mo@hotmail.com>
2014-10-06 21:29:03 -0300

    Creating the method to trace route in controller

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -9,16 +9,16 @@ class RoutesController < ApplicationController
 
 	end
 
-	def create
-		
-		@route = Route.new(origin_params)
+  	def trace_route
+
+  		@route = Route.new(origin_params)
 
 		@origin_informed_by_user = @route.origin
 		@destination_informed_by_user = @route.destination
 
 		render :index
-	
   	end
+
    	
 	def origin_params
   		
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     collection { post :import }
   end
 
-   get '/routes/trace' => 'routes#trace', as: :trace
+   post '/routes/trace' => 'routes#trace_route', as: :trace
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
