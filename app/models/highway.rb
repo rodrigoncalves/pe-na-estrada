5daa017b1a9352bcf11cb616d2d48bc1d0f64845
Leonardo Cambraia <leoccambraia@yahoo.com.br>
2014-10-09 13:22:09 -0300

    Merge branch 'testes_italo' of github.com:rodrigo14/pe-na-estrada into dev

--- app/models/highway.rb
+++ app/models/highway.rb
@@ -5,24 +5,21 @@ class Highway < ActiveRecord::Base
   validates_uniqueness_of :idBr, message: "This idBr already exists on data base."
 
   # Change this if the type of mileage change on schema
-  validates_numericality_of :mileage, greater_than_or_equal_to: 1, allow_nil: true, message: 'Mileage cannot be shorter than 1.' 
+  validates_numericality_of :mileage, greater_than_or_equal_to: 1, allow_nil: true, message: 'Mileage cannot be shorter than 1.'
 
 
   def self.search_for_highway id_to_search
-      
       if id_to_search.blank?
         id_to_search = ""
       else
         # Nothing to do
       end
-      
-      # Return all highways registered if id_to_search is empty
-      where("idBr LIKE ?", "%#{id_to_search}%")
 
+      # Return all highways registered if id_to_search is empty
+      where("idBr LIKE ?", "%#{id_to_search}%").order(:idBr)
   end
 
   def self.exists_highway highway_to_check
-
     if highway_to_check.present?
       exists?(['idBr LIKE ?', "%#{highway_to_check}%"])
     else
@@ -32,15 +29,11 @@ class Highway < ActiveRecord::Base
   end
 
   def self.all_highways_by_accidentsRate
-
       all.order(:accidentsRate).reverse_order
-
   end
 
   def self.all_highways_by_accidentsRatePercent
-
       all.order(:accidentsRatePercent).reverse_order
-
   end
-    
+
 end
