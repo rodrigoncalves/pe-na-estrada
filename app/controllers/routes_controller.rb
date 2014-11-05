9edd0ce31ca1593ed37c9ac50858f89974d2ffb0
italopaiva <italosensei@hotmail.com>
2014-11-05 10:58:39 -0200

    Atomizing methods to sinalize acccidents on routes controller

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -11,21 +11,27 @@ class RoutesController < ApplicationController
     @origin_informed_by_user = @route.origin
     @destination_informed_by_user = @route.destination
 
+    get_accidents_data_to_sinalize
 
+    render :index
+  end
+
+  def get_accidents_data_to_sinalize
     @latitude = Accident.get_accidents_latitude
     @longitude = Accident.get_accidents_longitude
     @br = Accident.get_accidents_br
 
-    gon.latitude = @latitude
-    gon.longitude = @longitude
-    gon.br = @br
+    send_accidents_data_to_js @latitude, @longitude, @br
+  end
 
-    render :index
+  def send_accidents_data_to_js latitude, longitude, br
+    gon.latitude = latitude
+    gon.longitude = longitude
+    gon.br = br
   end
 
   def origin_params
     params.require(:route).permit(:origin, :destination)
   end
-
 
 end
