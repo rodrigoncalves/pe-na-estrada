f09412fd52837077ca2f84268d8154b18f249aa9
Emilie Morais <emilie.mo@hotmail.com>
2014-11-16 16:02:17 -0200

    Removing the unusable coordinates

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -17,11 +17,36 @@ class RoutesController < ApplicationController
   end
 
   def get_accidents_data_to_sinalize
-    @latitude = Accident.get_accidents_latitude
-    @longitude = Accident.get_accidents_longitude
-    @br = Accident.get_accidents_br
 
-    send_accidents_data_to_js @latitude, @longitude, @br
+    latitude = Accident.get_accidents_latitude
+    longitude = Accident.get_accidents_longitude
+    br = Accident.get_accidents_br
+
+    remove_unusable_coordinates latitude, longitude, br
+  end
+
+  def remove_unusable_coordinates latitude, longitude, br
+    # Arrays to store the usables coordinates and highways
+    latitudeUsable = [];
+    longitudeUsable = [];
+    highways = [];
+
+    i = 0; #Count to get the latitudes from database
+    j = 0; #Get the latitude, longitude e br usables
+  
+    while i < latitude.length
+
+      if latitude[i].blank? == false
+        latitudeUsable[j] = latitude[i]
+        longitudeUsable[j] = longitude[i]
+        highways[j] = br[i]
+        j = j + 1
+      end
+
+      i = i + 1
+    end
+
+    send_accidents_data_to_js latitudeUsable, longitudeUsable, highways
   end
 
   def send_accidents_data_to_js latitude, longitude, br
