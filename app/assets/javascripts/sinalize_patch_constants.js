f9951a3c486864baa03e12f84b52408a571f9dd8
italopaiva <italosensei@hotmail.com>
2014-11-16 15:36:11 -0200

    Validating quantityOfPatches, creating documentation to functions and adjustments on sinalize_patch.js file

--- app/assets/javascripts/sinalize_patch_constants.js
+++
@@ -1,6 +0,0 @@
-
-const DEFAULT_QUANTITY_OF_PATCHES = 10;
-
-// Error constants
-const QUANTITY_OF_PATCHES_IS_ZERO = "quantityOfPatches cannot be 0";
-const QUANTITY_OF_PATCHES_IS_NEGATIVE = "quantityOfPatches cannot be negative";
